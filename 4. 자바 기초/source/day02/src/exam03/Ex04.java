1504521d2c6bc5e5b94400102f784d0260c861cc
JINI-0411 <aricclunj@naver.com>
2024-10-08 18:28:19 +0900

    day02 수업 소스

--- 4. 자바 기초/source/day02/src/exam03/Ex04.java
+++ 4. 자바 기초/source/day02/src/exam03/Ex04.java
@@ -3,7 +3,7 @@ package exam03;
 public class Ex04 {
     public static void main(String[] args) {
         int total = 0;
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= 100; i++) { //
 //            if (i == 50){
 //                continue; // 현재 반복 중단, 새로 반복 시작 - 건너 뛰기
 //            }
